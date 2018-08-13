24f9aec5b6fa6ecda6036fa48786e751c0af6b18
vis2k <info@noobtuts.com>
2018-08-13 17:46:46 +0200

    Server.Listen function to reduce complexity

--- Telepathy/Server.cs
+++ Telepathy/Server.cs
@@ -21,6 +21,66 @@ namespace Telepathy
             get { return listenerThread != null && listenerThread.IsAlive; }
         }
 
+        // the listener thread's listen function
+        void Listen(int port)
+        {
+            // absolutely must wrap with try/catch, otherwise thread
+            // exceptions are silent
+            try
+            {
+                // start listener
+                listener = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+                listener.Start();
+                Logger.Log("Server is listening");
+
+                // keep accepting new clients
+                while (true)
+                {
+                    // wait and accept new client
+                    // note: 'using' sucks here because it will try to
+                    // dispose after thread was started but we still need it
+                    // in the thread
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    // generate the next connection id (thread safely)
+                    uint connectionId = counter.Next();
+
+                    // spawn a thread for each client to listen to his
+                    // messages
+                    Thread thread = new Thread(() =>
+                    {
+                        // add to dict immediately
+                        clients.Add(connectionId, client);
+
+                        // run the receive loop
+                        ReceiveLoop(connectionId, client);
+
+                        // remove client from clients dict afterwards
+                        clients.Remove(connectionId);
+                    });
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
+            }
+            catch (ThreadAbortException exception)
+            {
+                // UnityEditor causes AbortException if thread is still
+                // running when we press Play again next time. that's okay.
+                Logger.Log("Server thread aborted. That's okay. " + exception);
+            }
+            catch (SocketException exception)
+            {
+                // calling StopServer will interrupt this thread with a
+                // 'SocketException: interrupted'. that's okay.
+                Logger.Log("Server Thread stopped. That's okay. " + exception);
+            }
+            catch (Exception exception)
+            {
+                // something went wrong. probably important.
+                Logger.LogError("Server Exception: " + exception);
+            }
+        }
+
         // start listening for new connections in a background thread and spawn
         // a new thread for each one.
         public void Start(int port)
@@ -36,64 +96,7 @@ namespace Telepathy
 
             // start the listener thread
             Logger.Log("Server: starting on port=" + port);
-            listenerThread = new Thread(() =>
-            {
-                // absolutely must wrap with try/catch, otherwise thread
-                // exceptions are silent
-                try
-                {
-                    // start listener
-                    listener = new TcpListener(new IPEndPoint(IPAddress.Any, port));
-                    listener.Start();
-                    Logger.Log("Server is listening");
-
-                    // keep accepting new clients
-                    while (true)
-                    {
-                        // wait and accept new client
-                        // note: 'using' sucks here because it will try to
-                        // dispose after thread was started but we still need it
-                        // in the thread
-                        TcpClient client = listener.AcceptTcpClient();
-
-                        // generate the next connection id (thread safely)
-                        uint connectionId = counter.Next();
-
-                        // spawn a thread for each client to listen to his
-                        // messages
-                        Thread thread = new Thread(() =>
-                        {
-                            // add to dict immediately
-                            clients.Add(connectionId, client);
-
-                            // run the receive loop
-                            ReceiveLoop(connectionId, client);
-
-                            // remove client from clients dict afterwards
-                            clients.Remove(connectionId);
-                        });
-                        thread.IsBackground = true;
-                        thread.Start();
-                    }
-                }
-                catch (ThreadAbortException exception)
-                {
-                    // UnityEditor causes AbortException if thread is still
-                    // running when we press Play again next time. that's okay.
-                    Logger.Log("Server thread aborted. That's okay. " + exception);
-                }
-                catch (SocketException exception)
-                {
-                    // calling StopServer will interrupt this thread with a
-                    // 'SocketException: interrupted'. that's okay.
-                    Logger.Log("Server Thread stopped. That's okay. " + exception);
-                }
-                catch (Exception exception)
-                {
-                    // something went wrong. probably important.
-                    Logger.LogError("Server Exception: " + exception);
-                }
-            });
+            listenerThread = new Thread(() => { Listen(port); });
             listenerThread.IsBackground = true;
             listenerThread.Start();
         }
