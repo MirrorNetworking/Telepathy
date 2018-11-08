300e13e00814f6bed856d85be96aa1b8e706d07b
Paul Pacheco <paulpach@gmail.com>
2018-11-08 13:42:32 -0600

    Fix project metadata

--- Telepathy/Properties/AssemblyInfo.cs
+++ Telepathy/Properties/AssemblyInfo.cs
@@ -4,8 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
