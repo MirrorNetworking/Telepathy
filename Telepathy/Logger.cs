67c2d82dca5930b5d0600cd898be7f93622ab1d1
vis2k <info@noobtuts.com>
2019-01-27 17:01:28 +0100

    Logger

---
+++ Telepathy/Logger.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telepathy
+{
+    public static class Logger
+    {
+        public static Action<string> Log = Console.WriteLine;
+        public static Action<string> LogWarning = Console.WriteLine;
+        public static Action<string> LogError = Console.Error.WriteLine;
+    }
+}
