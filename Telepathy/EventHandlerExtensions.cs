78f7fee98e00b205ef9ef36f03de1ed48e23ba78
vis2k <info@noobtuts.com>
2019-01-27 18:26:26 +0100

    remove unused extensions

--- Telepathy/EventHandlerExtensions.cs
+++ Telepathy/EventHandlerExtensions.cs
@@ -2,19 +2,6 @@ using System;
 
 namespace Telepathy
 {
-    public static class EventHandlerExtensions
-    {
-        public static EventArgs<T> CreateArgs<T>(this EventHandler<EventArgs<T>> _,T argument)
-        {
-            return new EventArgs<T>(argument);
-        }
-
-        public static EventArgs<T,U> CreateArgs<T,U>(this EventHandler<EventArgs<T,U>> _, T v1, U v2)
-        {
-            return new EventArgs<T,U>(v1, v2);
-        }
-    }
-
     public class EventArgs<T> : EventArgs
     {
         public EventArgs(T value)
