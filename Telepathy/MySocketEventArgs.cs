493949d85097e2e487bda1a2aa60fb41f813e2d1
vis2k <info@noobtuts.com>
2019-01-27 16:28:00 +0100

    simplify MySocketEventArgs

--- Telepathy/MySocketEventArgs.cs
+++ Telepathy/MySocketEventArgs.cs
@@ -4,14 +4,7 @@ namespace Telepathy
 {
     class MySocketEventArgs : SocketAsyncEventArgs
     {
-        /// <summary>
-        /// 标识，只是一个编号而已
-        /// </summary>
-        public int ArgsTag { get; set; }
-
-        /// <summary>
-        /// 设置/获取使用状态
-        /// </summary>
-        public bool IsUsing { get; set; }
+        public int ArgsTag;
+        public bool IsUsing;
     }
 }
