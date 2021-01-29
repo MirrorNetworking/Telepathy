2b64c95bf188d342abe83e9ce3d00e1b00b2d54f
vis2k <info@noobtuts.com>
2021-01-29 20:55:41 +0800

    remove unused Utils.IntToBytesBigEndian

--- Telepathy/Utils.cs
+++ Telepathy/Utils.cs
@@ -2,26 +2,6 @@ namespace Telepathy
 {
     public static class Utils
     {
-        // fast int to byte[] conversion and vice versa
-        // -> test with 100k conversions:
-        //    BitConverter.GetBytes(ushort): 144ms
-        //    bit shifting: 11ms
-        // -> 10x speed improvement makes this optimization actually worth it
-        // -> this way we don't need to allocate BinaryWriter/Reader either
-        // -> 4 bytes because some people may want to send messages larger than
-        //    64K bytes
-        // => big endian is standard for network transmissions, and necessary
-        //    for compatibility with erlang
-        public static byte[] IntToBytesBigEndian(int value)
-        {
-            return new byte[] {
-                (byte)(value >> 24),
-                (byte)(value >> 16),
-                (byte)(value >> 8),
-                (byte)value
-            };
-        }
-
         // IntToBytes version that doesn't allocate a new byte[4] each time.
         // -> important for MMO scale networking performance.
         public static void IntToBytesBigEndianNonAlloc(int value, byte[] bytes)
