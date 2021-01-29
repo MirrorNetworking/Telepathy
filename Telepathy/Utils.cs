30e25f11f42f8dc20c5868f909c9b12003973a95
vis2k <info@noobtuts.com>
2021-01-29 20:57:13 +0800

    syntax

--- Telepathy/Utils.cs
+++ Telepathy/Utils.cs
@@ -14,11 +14,10 @@ namespace Telepathy
 
         public static int BytesToIntBigEndian(byte[] bytes)
         {
-            return
-                (bytes[0] << 24) |
-                (bytes[1] << 16) |
-                (bytes[2] << 8) |
-                bytes[3];
+            return (bytes[0] << 24) |
+                   (bytes[1] << 16) |
+                   (bytes[2] << 8) |
+                    bytes[3];
         }
     }
 }
