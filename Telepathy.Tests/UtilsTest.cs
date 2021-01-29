2520118f36b77a9670656ac606fbcdbae2579753
vis2k <info@noobtuts.com>
2021-01-29 22:59:03 +0800

    simplify tests

--- Telepathy.Tests/UtilsTest.cs
+++ Telepathy.Tests/UtilsTest.cs
@@ -9,15 +9,12 @@ namespace Telepathy.Tests
         {
             int number = 0x01020304;
 
-            byte[] numberBytes = new byte[4];
-            Utils.IntToBytesBigEndianNonAlloc(number, numberBytes);
-            Assert.That(numberBytes[0], Is.EqualTo(0x01));
-            Assert.That(numberBytes[1], Is.EqualTo(0x02));
-            Assert.That(numberBytes[2], Is.EqualTo(0x03));
-            Assert.That(numberBytes[3], Is.EqualTo(0x04));
-
-            int converted = Utils.BytesToIntBigEndian(numberBytes);
-            Assert.That(converted, Is.EqualTo(number));
+            byte[] bytes = new byte[4];
+            Utils.IntToBytesBigEndianNonAlloc(number, bytes);
+            Assert.That(bytes[0], Is.EqualTo(0x01));
+            Assert.That(bytes[1], Is.EqualTo(0x02));
+            Assert.That(bytes[2], Is.EqualTo(0x03));
+            Assert.That(bytes[3], Is.EqualTo(0x04));
         }
 
         [Test]
@@ -25,14 +22,14 @@ namespace Telepathy.Tests
         {
             int number = 0x01020304;
 
-            byte[] numberBytes = {0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00};
-            Utils.IntToBytesBigEndianNonAlloc(number, numberBytes, 2);
-            Assert.That(numberBytes[0], Is.EqualTo(0xFF));
-            Assert.That(numberBytes[1], Is.EqualTo(0xFF));
-            Assert.That(numberBytes[2], Is.EqualTo(0x01));
-            Assert.That(numberBytes[3], Is.EqualTo(0x02));
-            Assert.That(numberBytes[4], Is.EqualTo(0x03));
-            Assert.That(numberBytes[5], Is.EqualTo(0x04));
+            byte[] bytes = {0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00};
+            Utils.IntToBytesBigEndianNonAlloc(number, bytes, 2);
+            Assert.That(bytes[0], Is.EqualTo(0xFF));
+            Assert.That(bytes[1], Is.EqualTo(0xFF));
+            Assert.That(bytes[2], Is.EqualTo(0x01));
+            Assert.That(bytes[3], Is.EqualTo(0x02));
+            Assert.That(bytes[4], Is.EqualTo(0x03));
+            Assert.That(bytes[5], Is.EqualTo(0x04));
         }
     }
 }
