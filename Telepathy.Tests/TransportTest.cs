bd54d3be9ec8b2cd03c2bab9ddb4421f9324f649
vis2k <info@noobtuts.com>
2021-01-29 20:54:40 +0800

    UtilsTest

--- Telepathy.Tests/TransportTest.cs
+++ Telepathy.Tests/TransportTest.cs
@@ -350,37 +350,6 @@ namespace Telepathy.Tests
             }
         }
 
-        [Test]
-        public void IntToBytesBigTest()
-        {
-            int number = 0x01020304;
-
-            byte[] numberBytes = Utils.IntToBytesBigEndian(number);
-            Assert.That(numberBytes[0], Is.EqualTo(0x01));
-            Assert.That(numberBytes[1], Is.EqualTo(0x02));
-            Assert.That(numberBytes[2], Is.EqualTo(0x03));
-            Assert.That(numberBytes[3], Is.EqualTo(0x04));
-
-            int converted = Utils.BytesToIntBigEndian(numberBytes);
-            Assert.That(converted, Is.EqualTo(number));
-        }
-
-        [Test]
-        public void IntToBytesBigNonAllocTest()
-        {
-            int number = 0x01020304;
-
-            byte[] numberBytes = new byte[4];
-            Utils.IntToBytesBigEndianNonAlloc(number, numberBytes);
-            Assert.That(numberBytes[0], Is.EqualTo(0x01));
-            Assert.That(numberBytes[1], Is.EqualTo(0x02));
-            Assert.That(numberBytes[2], Is.EqualTo(0x03));
-            Assert.That(numberBytes[3], Is.EqualTo(0x04));
-
-            int converted = Utils.BytesToIntBigEndian(numberBytes);
-            Assert.That(converted, Is.EqualTo(number));
-        }
-
         static Message NextMessage(Server server)
         {
             Message message;
