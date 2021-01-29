5f62c4692b23d6dbaf989d69c8694be673db59a7
vis2k <info@noobtuts.com>
2021-01-29 22:56:57 +0800

    Utils.IntToBytesBigEndianNonAlloc: offset parameter added

--- Telepathy.Tests/UtilsTest.cs
+++ Telepathy.Tests/UtilsTest.cs
@@ -19,5 +19,20 @@ namespace Telepathy.Tests
             int converted = Utils.BytesToIntBigEndian(numberBytes);
             Assert.That(converted, Is.EqualTo(number));
         }
+
+        [Test]
+        public void IntToBytesBigNonAllocWithOffsetTest()
+        {
+            int number = 0x01020304;
+
+            byte[] numberBytes = {0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00};
+            Utils.IntToBytesBigEndianNonAlloc(number, numberBytes, 2);
+            Assert.That(numberBytes[0], Is.EqualTo(0xFF));
+            Assert.That(numberBytes[1], Is.EqualTo(0xFF));
+            Assert.That(numberBytes[2], Is.EqualTo(0x01));
+            Assert.That(numberBytes[3], Is.EqualTo(0x02));
+            Assert.That(numberBytes[4], Is.EqualTo(0x03));
+            Assert.That(numberBytes[5], Is.EqualTo(0x04));
+        }
     }
 }
--- Telepathy/Utils.cs
+++ Telepathy/Utils.cs
@@ -4,12 +4,12 @@ namespace Telepathy
     {
         // IntToBytes version that doesn't allocate a new byte[4] each time.
         // -> important for MMO scale networking performance.
-        public static void IntToBytesBigEndianNonAlloc(int value, byte[] bytes)
+        public static void IntToBytesBigEndianNonAlloc(int value, byte[] bytes, int offset = 0)
         {
-            bytes[0] = (byte)(value >> 24);
-            bytes[1] = (byte)(value >> 16);
-            bytes[2] = (byte)(value >> 8);
-            bytes[3] = (byte)value;
+            bytes[offset + 0] = (byte)(value >> 24);
+            bytes[offset + 1] = (byte)(value >> 16);
+            bytes[offset + 2] = (byte)(value >> 8);
+            bytes[offset + 3] = (byte)value;
         }
 
         public static int BytesToIntBigEndian(byte[] bytes)
