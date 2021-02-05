35ab1677365a0b610fb92831493e5c66221f9a76
vis2k <info@noobtuts.com>
2021-02-05 09:23:07 +0800

    fix: SendThread simply uses Peek & Dequeue instead of the SendPipe.DequeueAndSerializeAll insanity. fixes flaky tests that started being flaky after previous ConcurrentQueue/Stack commit. A lot easier too.

--- Telepathy.Tests/MagnificentSendPipeTests.cs
+++ Telepathy.Tests/MagnificentSendPipeTests.cs
@@ -25,41 +25,6 @@ namespace Telepathy.Tests
         }
 
         [Test]
-        public void DequeueAndSerializeAll()
-        {
-            // enqueue two
-            pipe.Enqueue(new ArraySegment<byte>(new byte[]{0xAA}));
-            pipe.Enqueue(new ArraySegment<byte>(new byte[]{0xBB, 0xCC}));
-
-            // pass an empty payload. DequeueAll should initialize / scale it!
-            byte[] payload = null;
-
-            // dequeue and serialize all
-            bool result = pipe.DequeueAndSerializeAll(ref payload, out int packetSize);
-            Assert.That(result, Is.True);
-            // header + content, header + content
-            Assert.That(packetSize, Is.EqualTo(4+1 + 4+2));
-            // first header
-            Assert.That(payload[0], Is.EqualTo(0x00));
-            Assert.That(payload[1], Is.EqualTo(0x00));
-            Assert.That(payload[2], Is.EqualTo(0x00));
-            Assert.That(payload[3], Is.EqualTo(0x01));
-            // first content
-            Assert.That(payload[4], Is.EqualTo(0xAA));
-            // second header
-            Assert.That(payload[5], Is.EqualTo(0x00));
-            Assert.That(payload[6], Is.EqualTo(0x00));
-            Assert.That(payload[7], Is.EqualTo(0x00));
-            Assert.That(payload[8], Is.EqualTo(0x02));
-            // second content
-            Assert.That(payload[9], Is.EqualTo(0xBB));
-            Assert.That(payload[10], Is.EqualTo(0xCC));
-
-            // pipe should be empty now
-            Assert.That(pipe.Count, Is.EqualTo(0));
-        }
-
-        [Test]
         public void Clear()
         {
             pipe.Enqueue(new ArraySegment<byte>(new byte[]{0x1}));
@@ -80,9 +45,8 @@ namespace Telepathy.Tests
             pipe.Enqueue(new ArraySegment<byte>(new byte[]{0x1}));
             Assert.That(pipe.PoolCount, Is.EqualTo(0));
 
-            // dequeue all. should return the byte[] to the pool
-            byte[] payload = null;
-            pipe.DequeueAndSerializeAll(ref payload, out int _);
+            // dequeue. should return the byte[] to the pool
+            pipe.TryDequeue();
             Assert.That(pipe.PoolCount, Is.EqualTo(1));
 
             // enqueue one. should use the pooled entry
